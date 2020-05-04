1d4cef2ecc6fac8c405b3fd2f25fc460f37fdd4f
Noam Rotem <noam.rotem@thomsonreuters.com>
2020-05-04 19:25:32 +0300

    #1 - making the workflow sort of work, but it doesn't flow

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
+import os
+
 import requests
-from flask import Flask, Response
+from flask import Flask, Response, send_from_directory
 from flask_mako import MakoTemplates, render_template
-from flask_cors import CORS
+# from flask_cors import CORS
 
 app = Flask(__name__)
 app.template_folder = "templates"
-cors = CORS(app, resources={r"/*": {"origins": "https://github.com"}})
+# cors = CORS(app, resources={r"/*": {"origins": "https://github.com"}})
 
 mako = MakoTemplates(app)
 
@@ -15,20 +17,29 @@ def login_form():
     return render_template('login.html', name='mako')
 
 
+@app.route('/login', methods=['POST'])
+def login():
+    return render_template('post_login.html', name='mako')
+
 @app.route('/signum-bundle.min.js')
 def get_signum():
-    resp = requests.get("https://cdn.jsdelivr.net/gh/metormaon/signum-js@master/js/signum-bundle.min.js")
+    resp = requests.get("https://raw.githubusercontent.com/metormaon/signum-js/noam-adapt-login-function-to-reality/js/signum-bundle.min.js")
 
-    return Response(resp.content, resp.status_code, {"Content-type": "application/javascript"})
+    return Response(resp.content, resp.status_code, {"Content-type": "application/javascript", "Cache-Control": "no-cache"})
 
 
 @app.route('/signum-bundle.min.js.map')
 def get_signum_map():
-    resp = requests.get("https://cdn.jsdelivr.net/gh/metormaon/signum-js@master/js/signum-bundle.min.js.map")
+    resp = requests.get("https://raw.githubusercontent.com/metormaon/signum-js/noam-adapt-login-function-to-reality/js/signum-bundle.min.js.map")
 
-    return Response(resp.content, resp.status_code, {"Content-typr": "application/javascript"})
+    return Response(resp.content, resp.status_code, {"Content-type": "application/json", "Cache-Control": "no-cache"})
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/x-icon')
 
 
 if __name__ == '__main__':
     # app.run()
-    app.run(debug=True, use_debugger=False, use_reloader=False, passthrough_errors=True)
+    app.run(debug=True, use_debugger=False, use_reloader=False, passthrough_errors=False)
