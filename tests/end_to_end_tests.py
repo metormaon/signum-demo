5765a780fa634abdceafae2d136405e85b5a4466
Noam Rotem <noam.rotem@thomsonreuters.com>
2020-08-03 15:50:17 +0300

    Playing some more with test mode

--- tests/end_to_end_tests.py
+++ tests/end_to_end_tests.py
@@ -9,7 +9,7 @@ from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as EC, expected_conditions
 
 # noinspection PyUnresolvedReferences
 import chromedriver_binary
@@ -19,33 +19,33 @@ class TestLogin(unittest.TestCase):
     container = None
     process = None
 
-    @classmethod
-    def setUpClass(cls):
-        if os.name != 'nt':
-            client = docker.from_env()
-
-            for previous_container in client.containers.list(filters={
-                                                    "ancestor": "docker.pkg.github.com/metormaon/signum-demo/demo:merge",
-                                                    "status": "running"}):
-                previous_container.kill()
-
-            cls.container = client.containers.run("docker.pkg.github.com/metormaon/signum-demo/demo:merge",
-                                                  detach=True,
-                                                  ports={"5000": "5000"}, environment=["SIGNUM_TEST_MODE=True"])
-        else:
-            cls.subprocess = subprocess.Popen("python ../app.py", shell=True, env={"SIGNUM_TEST_MODE": "True"})
-
-    @classmethod
-    def tearDownClass(cls):
-        if cls.container:
-            cls.container.kill()
-
-        if cls.process:
-            cls.process.kill()
+    # @classmethod
+    # def setUpClass(cls):
+    #     if os.name != 'nt':
+    #         client = docker.from_env()
+    #
+    #         for previous_container in client.containers.list(filters={
+    #                                                 "ancestor": "docker.pkg.github.com/metormaon/signum-demo/demo:noam-enhancements",
+    #                                                 "status": "running"}):
+    #             previous_container.kill()
+    #
+    #         cls.container = client.containers.run("docker.pkg.github.com/metormaon/signum-demo/demo:noam-enhancements",
+    #                                               detach=True,
+    #                                               ports={"5000": "5000"}, environment=["SIGNUM_TEST_MODE=True"])
+    #     else:
+    #         cls.subprocess = subprocess.Popen("python ../app.py", shell=True, env={"SIGNUM_TEST_MODE": "True"})
+    #
+    # @classmethod
+    # def tearDownClass(cls):
+    #     if cls.container:
+    #         cls.container.kill()
+    #
+    #     if cls.process:
+    #         cls.process.kill()
 
     def setUp(self) -> None:
         self.browser = webdriver.Chrome()
-        self.browser.get("http://127.0.0.1:5000/")
+        self.browser.get("http://127.0.0.1:5000/signup")
         timeout = 3  # seconds
         try:
             WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((By.ID, 'username')))
@@ -55,31 +55,59 @@ class TestLogin(unittest.TestCase):
     def tearDown(self) -> None:
         self.browser.close()
 
-    def test_sign_up(self):
+    def test_sign_up_then_login(self):
         username = self.browser.find_element_by_id("username")
         username.clear()
         username.send_keys("test_user")
 
         password = self.browser.find_element_by_id("password")
         password.clear()
-        password.send_keys("123456")
+        password.send_keys("123456abcEE$")
 
-        state_text = self.browser.find_element_by_id("state").get_attribute("value")
+        password = self.browser.find_element_by_id("confirm_password")
+        password.clear()
+        password.send_keys("123456abcEE$")
 
-        state = json.loads(state_text)
+        captcha_options_text = self.browser.find_element_by_id("state").get_attribute("value")
 
-        captcha_solution = state["captcha_solutions"][0]
+        captcha_options = json.loads(captcha_options_text.replace("\'", "\""))
+
+        captcha_solution = captcha_options[0]
 
         captcha = self.browser.find_element_by_id("captcha")
         captcha.clear()
         captcha.send_keys(captcha_solution)
 
-        url = self.browser.current_url
+        submit = self.browser.find_element_by_id("submit")
+        submit.click()
+
+        WebDriverWait(self.browser, 30).until(
+            expected_conditions.url_contains("http://127.0.0.1:5000/post-signup")
+        )
+
+        self.browser.get("http://127.0.0.1:5000/")
+
+        username = self.browser.find_element_by_id("username")
+        username.clear()
+        username.send_keys("test_user")
+
+        password = self.browser.find_element_by_id("password")
+        password.clear()
+        password.send_keys("123456abcEE$")
+
+        captcha_options_text = self.browser.find_element_by_id("state").get_attribute("value")
+
+        captcha_options = json.loads(captcha_options_text.replace("\'", "\""))
+
+        captcha_solution = captcha_options[0]
+
+        captcha = self.browser.find_element_by_id("captcha")
+        captcha.clear()
+        captcha.send_keys(captcha_solution)
 
         submit = self.browser.find_element_by_id("submit")
         submit.click()
 
-        time.sleep(10)
-
-        assert self.browser.current_url == url
-        assert self.browser.find_element_by_id("errorMessage").is_displayed()
+        WebDriverWait(self.browser, 30).until(
+            expected_conditions.url_contains("http://127.0.0.1:5000/post-login")
+        )
